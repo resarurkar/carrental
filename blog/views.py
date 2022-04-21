4c415a6a9c9921b450b0c8348d27e6faf2cafa0e
resarurk <x21128197@student.ncirl.ie>
2022-04-21 13:21:06 +0100

    About.html updated

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render (request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Blog About>/h1/>')
+    return render (request, 'blog/about.html')
 
 # Create your views here.
