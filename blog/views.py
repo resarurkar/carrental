b0f242b5d43e151af6d4698222b97025f309c5e2
resarurk <x21128197@student.ncirl.ie>
2022-04-21 12:46:54 +0100

    about page changes

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Blog Home>/h1/>')
 
+def about(request):
+    return HttpResponse('<h1>Blog About>/h1/>')
+
 # Create your views here.
