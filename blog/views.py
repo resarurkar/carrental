a16d3966e5e7f86e1a1b4a5c9a22c1c6537dd8de
resarurk <x21128197@student.ncirl.ie>
2022-04-21 13:41:16 +0100

    posts updated

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,28 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Sudha Murthey',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted' : 'April 13, 2022'
+    },
+    {
+        'author': 'Jordan peterson',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted' : 'April 15, 2022'
+    }
+
+]
 
 
 def home(request):
-    return render (request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render (request, 'blog/home.html', context)
 
 def about(request):
     return render (request, 'blog/about.html')
