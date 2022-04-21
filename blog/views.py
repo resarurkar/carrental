2fec9b4bb9e2026b19b0e7f469b57129357cdb0e
resarurk <x21128197@student.ncirl.ie>
2022-04-21 14:17:06 +0100

    title commit

--- blog/views.py
+++ blog/views.py
@@ -25,6 +25,6 @@ def home(request):
     return render (request, 'blog/home.html', context)
 
 def about(request):
-    return render (request, 'blog/about.html')
+    return render (request, 'blog/about.html', {'title' : 'About'})
 
 # Create your views here.
