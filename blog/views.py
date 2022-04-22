b2c6b032cf8f7ddc3bf64e09a24681d47f4b366a
resarurk <x21128197@student.ncirl.ie>
2022-04-22 10:51:07 +0100

    update post

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from multiprocessing import context
 from re import template
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 
@@ -32,7 +32,13 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
         
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render (request, 'blog/about.html', {'title' : 'About'})
