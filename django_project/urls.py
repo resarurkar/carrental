84c41030f875a74aad8961c2d2523ca8e9acf955
resarurk <x21128197@student.ncirl.ie>
2022-04-22 11:56:50 +0100

    Password Reset

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('passowrd-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('passowrd-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('passowrd-reset-confirm/<uidb64/<token>>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('blog/', include('blog.urls')),
 ]
 
