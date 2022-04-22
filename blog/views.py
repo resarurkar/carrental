56e3585cf5b7a3606da722504110211ac571577c
resarurk <x21128197@student.ncirl.ie>
2022-04-22 11:04:22 +0100

    update

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from multiprocessing import context
 from re import template
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -45,6 +45,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render (request, 'blog/about.html', {'title' : 'About'})
